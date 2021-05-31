4459056788fee57340cd86a217e149feba0f4f9d
porto-dev <ns-512@yandex.ru>
2021-05-31 17:17:56 +0300

    added new

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,13 @@
 $(function(){
 
-
+ 
+  $('.rate-star').raty({ path: 'images', readOnly: true, score: 5});
+  var mixer = mixitup('.pruducts__inner-box');
+  $('.product-slider__inner').slick({ 
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
 });
+
