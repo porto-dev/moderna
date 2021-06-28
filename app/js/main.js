45faeaf9b4f0beb9bd0a4201fa49c9accc425293
porto-dev <ns-512@yandex.ru>
2021-06-28 12:05:22 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,23 @@ $(function(){
 
  
   $('.rate-star').raty({ path: 'images', readOnly: true, score: 5});
-  var mixer = mixitup('.pruducts__inner-box');
+  
   $('.product-slider__inner').slick({ 
     dots: true,
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4
   });
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active'); // добавление класса элементу и отмена добавления
+  });
+  
+
+  var mixer = mixitup('.pruducts__inner-box'); //пишется для mixitup обязательно в конце иначе не работает
 });
 
